2b4ab93fa3fd0dba4a70fd4b36e741388dc01caf
Kyle Tryon <Kyle.Tryon@outlook.com>
2020-09-04 11:33:37 -0400

    move install to script and update logic

---
+++ src/scripts/install.sh
@@ -0,0 +1,12 @@
+if echo "$OSTYPE "| grep darwin > /dev/null 2>&1; then
+    echo "Brew is installed"
+elif  grep Debian /etc/issue > /dev/null 2>&1 || grep Ubuntu /etc/issue > /dev/null 2>&1; then
+    echo "Debian"
+    if ! command -v brew >/dev/null 2>&1; then
+        curl -fsSL "https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh" | bash
+        /home/linuxbrew/.linuxbrew/bin/brew shellenv >> "$BASH_ENV"
+    fi
+    echo "Brew is installed"
+fi
+brew install aws/tap/aws-sam-cli
+sam --version
