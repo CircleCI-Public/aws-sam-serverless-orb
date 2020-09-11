88c9679b480787437593cb07801b08ce7529e407
Kyle Tryon <Kyle.Tryon@outlook.com>
2020-09-11 11:50:39 -0400

    no linuxbrew

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,16 +1,2 @@
-if echo "$OSTYPE "| grep darwin > /dev/null 2>&1; then
-    echo "Brew is installed"
-elif  grep Debian /etc/issue > /dev/null 2>&1 || grep Ubuntu /etc/issue > /dev/null 2>&1; then
-    echo "Debian"
-    if ! command -v brew >/dev/null 2>&1; then
-        curl -fsSL "https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh" | bash
-        /home/linuxbrew/.linuxbrew/bin/brew shellenv >> "$BASH_ENV"
-        source $BASH_ENV
-    fi
-    echo "Brew is installed"
-fi
-# Install twice as bug patch. See https://github.com/CircleCI-Public/aws-sam-serverless-orb/issues/20
-brew install python
 pip3 install aws-sam-cli
-
 sam --version
