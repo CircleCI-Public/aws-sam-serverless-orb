d87dd633c6429740fb396c51940edabaf8f047be
Kyle Tryon <Kyle.Tryon@outlook.com>
2020-09-15 09:25:04 -0400

    test using venv

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,4 +1,9 @@
 cd /opt/$USER/.pyenv/plugins/python-build/../.. && git pull && cd -
 pyenv install 3.8.5
+pip3 install --user virtualenv
+python3 -m venv env
+source env/bin/activate
 pip3 install aws-sam-cli
+sam --version
+deactivate
 sam --version
