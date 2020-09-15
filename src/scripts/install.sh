7c08e7ad681b0f49ccdaca4566176937e2532569
Kyle Tryon <Kyle.Tryon@outlook.com>
2020-09-15 15:46:26 -0400

    try with pip and docker

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,7 +1,2 @@
-cd /opt/$USER/.pyenv/plugins/python-build/../.. && git pull && cd -
-pyenv install 3.8.5
-pip3 install --user virtualenv
-python3 -m venv env
-source env/bin/activate
-pip3 install aws-sam-cli
+pip install aws-sam-cli --user
 sam --version
