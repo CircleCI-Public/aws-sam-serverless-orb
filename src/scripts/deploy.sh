38efeb677433379f535ca3c336bd57294869f47a
Kyle Tryon <Kyle.Tryon@outlook.com>
2021-01-21 15:24:14 -0500

    convert deploy command to script

---
+++ src/scripts/deploy.sh
@@ -0,0 +1,19 @@
+set -- "$@" --capabilities "$SAM_PARAM_CAPABILITIES"
+set -- "$@" --template-file "$SAM_PARAM_TEMPLATE"
+set -- "$@" --stack-name "$SAM_PARAM_STACK_NAME"
+set -- "$@" --region "$SAM_PARAM_AWS_REGION"
+
+if [ -n "$SAM_PARAM_PROFILE_NAME" ]; then
+    set -- "$@" --profile "$SAM_PARAM_PROFILE_NAME"
+fi
+if [ "$SAM_PARAM_DEBUG" = 1 ]; then
+    set -- "$@" --debug
+fi
+if [ "$SAM_PARAM_PARAMETER_NOFAIL" = 1 ]; then
+    set -- "$@" --no-fail-on-empty-changeset
+fi
+if [ -n "$SAM_PARAM_PARAMETER_OVERRIDES" ]; then
+    set -- "$@" --parameter-overrides "$SAM_PARAM_PARAMETER_OVERRIDES"
+fi
+
+sam deploy "$@"
