238240e31c7a600b7570830b4172852779776a31
Kyle Tryon <Kyle.Tryon@outlook.com>
2021-02-11 11:25:38 -0500

    add image repos to deploy

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -1,4 +1,20 @@
 set -o noglob
+set_image_repos() {
+    IFS=', ' read -r -a ARRAY_REPOSITORIES <<< "$(eval echo "$SAM_PARAM_IMAGE_REPO")"
+    REPOARRYLEN=${#ARRAY_REPOSITORIES[@]}
+    if [ "$REPOARRYLEN" = 1 ]; then
+        echo "DEBUG: Single image repo"
+        echo "DEBUG: ${ARRAY_REPOSITORIES[0]}"
+        set -- "$@" --image-repository "$(eval echo "${ARRAY_REPOSITORIES[0]}")"
+    else
+        for image in "${!ARRAY_REPOSITORIES[@]}"; do
+            echo "DEBUG: Multi image repo"
+            echo "Images: ${#ARRAY_REPOSITORIES[@]}"
+            echo "DEBUG: ${ARRAY_REPOSITORIES[image]}"
+            set -- "$@" --image-repositories "$(eval echo "${ARRAY_REPOSITORIES[image]}")"
+        done
+    fi
+}
 IFS=', ' read -r -a ARRAY_CAPABILITIES <<< "$SAM_PARAM_CAPABILITIES"
 echo "${ARRAY_CAPABILITIES[@]}"
 set -- "$@" --capabilities "${ARRAY_CAPABILITIES[@]}"
@@ -7,6 +23,10 @@ set -- "$@" --stack-name "$SAM_PARAM_STACK_NAME"
 TEMP_REGION="\$"$(echo $SAM_PARAM_AWS_REGION)""
 set -- "$@" --region "$(eval "echo $TEMP_REGION")"
 
+
+if [ -n "$SAM_PARAM_IMAGE_REPO" ]; then
+    set_image_repos
+fi
 if [ -n "$SAM_PARAM_PROFILE_NAME" ]; then
     set -- "$@" --profile "$SAM_PARAM_PROFILE_NAME"
 fi
--- src/scripts/package.sh
+++ src/scripts/package.sh
@@ -1,11 +1,26 @@
+set -o noglob
+set_image_repos() {
+    IFS=', ' read -r -a ARRAY_REPOSITORIES <<< "$(eval echo "$SAM_PARAM_IMAGE_REPO")"
+    REPOARRYLEN=${#ARRAY_REPOSITORIES[@]}
+    if [ "$REPOARRYLEN" = 1 ]; then
+        echo "DEBUG: Single image repo"
+        echo "DEBUG: ${ARRAY_REPOSITORIES[0]}"
+        set -- "$@" --image-repository "$(eval echo "${ARRAY_REPOSITORIES[0]}")"
+    else
+        for image in "${!ARRAY_REPOSITORIES[@]}"; do
+            echo "DEBUG: Multi image repo"
+            echo "Images: ${#ARRAY_REPOSITORIES[@]}"
+            echo "DEBUG: ${ARRAY_REPOSITORIES[image]}"
+            set -- "$@" --image-repositories "$(eval echo "${ARRAY_REPOSITORIES[image]}")"
+        done
+    fi
+}
 echo "DEBUG: $(eval echo "$SAM_PARAM_IMAGE_REPO")"
 if [ -n "$SAM_PARAM_S3_BUCKET" ] || [ -n "$SAM_PARAM_IMAGE_REPO" ]; then
-    set -o noglob
     set -- "$@" --profile "$SAM_PARAM_PROFILE"
 
     TEMP_REGION="\$"$(echo $SAM_PARAM_AWS_REGION)""
     set -- "$@" --region "$(eval "echo $TEMP_REGION")"
-
 
     if [ -n "$SAM_PARAM_S3_BUCKET" ]; then
         if [ -n "$SAM_PARAM_IMAGE_REPO" ]; then
@@ -17,29 +32,13 @@ if [ -n "$SAM_PARAM_S3_BUCKET" ] || [ -n "$SAM_PARAM_IMAGE_REPO" ]; then
         echo "DEBUG: image repo param selected"
         if [ -n "$SAM_PARAM_S3_BUCKET" ]; then
             echo " parameters.s3-bucket cannot be set if parameters.image-repository is also configured. Remove one of these options."
+            exit 1
         fi
-        IFS=', ' read -r -a ARRAY_REPOSITORIES <<< "$(eval echo "$SAM_PARAM_IMAGE_REPO")"
-        REPOARRYLEN=${#ARRAY_REPOSITORIES[@]}
-        if [ "$REPOARRYLEN" = 1 ]; then
-            echo "DEBUG: Single image repo"
-            echo "DEBUG: ${ARRAY_REPOSITORIES[0]}"
-            set -- "$@" --image-repository "$(eval echo "${ARRAY_REPOSITORIES[0]}")"
-        else
-            for image in "${!ARRAY_REPOSITORIES[@]}"; do
-                echo "DEBUG: Multi image repo"
-                echo "Images: ${#ARRAY_REPOSITORIES[@]}"
-                echo "DEBUG: ${ARRAY_REPOSITORIES[image]}"
-                set -- "$@" --image-repositories "$(eval echo "${ARRAY_REPOSITORIES[image]}")"
-            done
-        fi
+        set_image_repos
     else
         set -- "$@" --template-file "$(eval "echo $SAM_PARAM_TEMPLATE")"
     fi
 
-
-    if [ -n "$SAM_PARAM_OUTPUT_TEMPLATE" ]; then
-        set -- "$@" --output-template-file "$(eval "echo $SAM_PARAM_OUTPUT_TEMPLATE")"
-    fi
     if [ "$SAM_PARAM_DEBUG" = 1 ]; then
         set -- "$@" --debug
     fi
