06236c13aedc4ff53acbcd853a5bd1f9cf3534cf
Kyle Tryon <Kyle.Tryon@outlook.com>
2021-03-17 16:02:46 -0400

    save path

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,9 +1,10 @@
-cd /tmp
+cd /tmp || true
 if [[ $EUID == 0 ]]; then export SUDO=""; else # Check if we are root
   export SUDO="sudo";
 fi
 curl -L https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip -o aws-sam-cli-linux-x86_64.zip
 unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
 $SUDO ./sam-installation/install
-where sam
+which sam
+echo "export PATH=$PATH:/usr/local/bin/sam" >> "$BASH_ENV"
 sam --version
