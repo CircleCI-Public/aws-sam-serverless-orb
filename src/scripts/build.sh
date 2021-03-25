1964e655e376214a1203ce4d4ff1218a57bca8c9
Kyle T <33272306+KyleTryon@users.noreply.github.com>
2021-03-25 10:33:08 -0400

    [semver:major] Lambda container support (#25)
    The packaging step has been removed as it is now a part of the deployment step.
    Deployment now accepts either s3-bucket for Lamda functions, or image-repositories for Lambda containers.
    The new native linux installer has replaced the pip based installer
    Usage examples updated to show version 3.0
    New lambda container example
    Orb now utilizes script include feature from orb tools 10
    Breaking changes:
    
    Capabilities parameter must now be provided as a comma-separated list, rather than multiple quoted strings within a single quote.
    Example:
    Old: capabilities: '"CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"'
    New: capabilities: CAPABILITY_IAM, CAPABILITY_NAMED_IAM
    Updated executor image
    New Features:
    
    image-repository parameter.
    Enables the deploy command work with ECR containers.
    Executor is parameterized with machine-image.
    Solves
    
    #18
    #24

---
+++ src/scripts/build.sh
@@ -0,0 +1,23 @@
+set -o noglob
+
+set -- "$@" --profile "$SAM_PARAM_PROFILE"
+
+set -- "$@" --template-file "$(eval "echo $SAM_PARAM_TEMPLATE")"
+
+TEMP_REGION="\$"$(echo $SAM_PARAM_AWS_REGION)""
+set -- "$@" --region "$(eval "echo $TEMP_REGION")"
+
+if [ -n "$SAM_PARAM_BUILD_DIR" ]; then
+  set -- "$@" --build-dir "$(eval "echo $SAM_PARAM_BUILD_DIR")"
+fi
+if [ -n "$SAM_PARAM_BASE_DIR" ]; then
+  set -- "$@" --base-dir "$(eval "echo $SAM_PARAM_BASE_DIR")"
+fi
+if [ "$SAM_PARAM_CONTAINER" = 1 ]; then
+  set -- "$@" --use-container
+fi
+if [ "$SAM_PARAM_DEBUG" = 1 ]; then
+  set -- "$@" --debug
+fi
+
+sam build "$@"
---
+++ src/scripts/deploy.sh
@@ -0,0 +1,54 @@
+set -o noglob
+IFS=', ' read -r -a ARRAY_CAPABILITIES <<< "$SAM_PARAM_CAPABILITIES"
+echo "${ARRAY_CAPABILITIES[@]}"
+set -- "$@" --capabilities "${ARRAY_CAPABILITIES[@]}"
+set -- "$@" --stack-name "$SAM_PARAM_STACK_NAME"
+
+TEMP_REGION="\$"$(echo $SAM_PARAM_AWS_REGION)""
+set -- "$@" --region "$(eval "echo $TEMP_REGION")"
+
+
+if [ -n "$SAM_PARAM_IMAGE_REPO" ]; then
+    if [ -n "$SAM_PARAM_S3_BUCKET" ]; then
+        echo " parameters.s3-bucket cannot be set if parameters.image-repository is also configured. Remove one of these options."
+        exit 1
+    fi
+    echo "DEBUG: set_image_repos called" "$(eval echo "$SAM_PARAM_IMAGE_REPO")"
+    IFS=', ' read -r -a ARRAY_REPOSITORIES <<< "$(eval echo "$SAM_PARAM_IMAGE_REPO")"
+    REPOARRYLEN=${#ARRAY_REPOSITORIES[@]}
+    if [ "$REPOARRYLEN" = 1 ]; then
+        echo "DEBUG: Single image repo"
+        echo "DEBUG: ${ARRAY_REPOSITORIES[0]}"
+        set -- "$@" --image-repository "$(eval echo "${ARRAY_REPOSITORIES[0]}")"
+    else
+        for image in "${!ARRAY_REPOSITORIES[@]}"; do
+            echo "DEBUG: Multi image repo"
+            echo "Images: ${#ARRAY_REPOSITORIES[@]}"
+            echo "DEBUG: ${ARRAY_REPOSITORIES[image]}"
+            set -- "$@" --image-repositories "$(eval echo "${ARRAY_REPOSITORIES[image]}")"
+        done
+    fi
+fi
+if [ -n "$SAM_PARAM_S3_BUCKET" ]; then
+    # Technically this shouldnt be needed as this shouldnt be possible given the order of execution
+    if [ -n "$SAM_PARAM_IMAGE_REPO" ]; then
+        echo " parameters.image-repository cannot be set if parameters.s3-bucket is also configured. Remove one of these options."
+    fi
+    set -- "$@" --s3-bucket "$SAM_PARAM_S3_BUCKET"
+fi
+if [ -n "$SAM_PARAM_PROFILE_NAME" ]; then
+    set -- "$@" --profile "$SAM_PARAM_PROFILE_NAME"
+fi
+if [ -n "$SAM_PARAM_TEMPLATE" ]; then
+    set -- "$@" --template-file "$(eval "echo $SAM_PARAM_TEMPLATE")"
+fi
+if [ "$SAM_PARAM_DEBUG" = 1 ]; then
+    set -- "$@" --debug
+fi
+if [ "$SAM_PARAM_PARAMETER_NOFAIL" = 1 ]; then
+    set -- "$@" --no-fail-on-empty-changeset
+fi
+if [ -n "$SAM_PARAM_PARAMETER_OVERRIDES" ]; then
+    set -- "$@" --parameter-overrides "$SAM_PARAM_PARAMETER_OVERRIDES"
+fi
+sam deploy "$@"
