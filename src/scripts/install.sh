38964b66d1c47265ddde50197d24d2b070fa255c
sambaiz <godgourd@gmail.com>
2021-06-04 23:21:57 +0900

    add version param to job and fix sam version in test

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -2,7 +2,7 @@ cd /tmp || true
 if [[ $EUID == 0 ]]; then export SUDO=""; else # Check if we are root
   export SUDO="sudo";
 fi
-if [ $SAM_PARAM_VERSION == "latest" ]; then
+if [[ $SAM_PARAM_VERSION == "latest" ]]; then
   curl -L https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip -o aws-sam-cli-linux-x86_64.zip
 else 
   curl -L https://github.com/aws/aws-sam-cli/releases/download/$SAM_PARAM_VERSION/aws-sam-cli-linux-x86_64.zip -o aws-sam-cli-linux-x86_64.zip
