9ce63f252251a6648995094eae5bc524161c12ad
KyleTryon <Kyle.Tryon@circleci.com>
2022-10-14 10:34:27 -0400

    feat: error with example for sam

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 cd /tmp || true
 if [[ $EUID == 0 ]]; then export SUDO=""; else # Check if we are root
-  export SUDO="sudo";
+  export SUDO="sudo"
+fi
+
+# Check if the AWS CLI is installed
+if ! command -v aws &>/dev/null; then
+  echo "AWS CLI could not be found. Please install it before initiating the SAM CLI."
+  echo
+  echo "Use the circleci/aws-cli orb to install the AWS CLI."
+  echo "https://circleci.com/developer/orbs/orb/circleci/aws-sam-serverless#usage-examples"
+  echo
+  echo "Job example:"
+  echo "
+    jobs:
+      build_app:
+        executor: sam/default
+        steps:
+          - checkout
+          - aws-cli/setup
+          - sam/install
+  "
+  echo
+  echo "Workflow example:"
+  echo "
+    workflows:
+      test_and_deploy:
+        jobs:
+          - sam/deploy:
+              pre-steps:
+                - aws-cli/setup
+  "
+  exit 1
 fi
 
 if [[ $SAM_PARAM_VERSION == "latest" ]]; then
@@ -14,5 +44,5 @@ fi
 unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
 $SUDO ./sam-installation/install
 which sam
-echo "export PATH=$PATH:/usr/local/bin/sam" >> "$BASH_ENV"
+echo "export PATH=$PATH:/usr/local/bin/sam" >>"$BASH_ENV"
 sam --version
