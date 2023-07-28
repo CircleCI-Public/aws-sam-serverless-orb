c278268d9cfc7b81010c645b3f8062f070037607
Brian Vu <emailbrian.vu@gmail.com>
2023-07-28 16:41:32 -0700

    feat: add build_parameter_overrides and build_arguments parameters

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -2,6 +2,8 @@
 set -o noglob
 ORB_STR_REGION="$(echo "${ORB_STR_REGION}" | circleci env subst)"
 ORB_STR_TEMPLATE="$(echo "${ORB_STR_TEMPLATE}" | circleci env subst)"
+ORB_STR_BUILD_PARAMETER_OVERRIDES="$(echo "${ORB_STR_BUILD_PARAMETER_OVERRIDES}" | circleci env subst)"
+ORB_STR_BUILD_ARGUMENTS="$(echo "${ORB_STR_BUILD_ARGUMENTS}" | circleci env subst)"
 ORB_EVAL_BUILD_DIR="$(eval echo "${ORB_EVAL_BUILD_DIR}" | circleci env subst)"
 ORB_EVAL_BASE_DIR="$(eval echo "${ORB_EVAL_BASE_DIR}" | circleci env subst)"
 
@@ -21,11 +23,11 @@ fi
 if [ "$ORB_BOOL_DEBUG" -eq 1 ]; then
   set -- "$@" --debug
 fi
-if [ -n "$SAM_PARAM_PARAMETER_OVERRIDES" ]; then
-    set -- "$@" --parameter-overrides "$(eval "echo $SAM_PARAM_PARAMETER_OVERRIDES")"
+if [ -n "$ORB_STR_BUILD_PARAMETER_OVERRIDES" ]; then
+    set -- "$@" --parameter-overrides "${ORB_STR_BUILD_PARAMETER_OVERRIDES}"
 fi
-if [ -n "$SAM_PARAM_ADDITIONAL_ARGS" ]; then
-    set -- "$@" "$SAM_PARAM_ADDITIONAL_ARGS"
+if [ -n "$ORB_STR_BUILD_ARGUMENTS" ]; then
+    set -- "$@" "${ORB_STR_BUILD_ARGUMENTS}"
 fi
 
 set -x
