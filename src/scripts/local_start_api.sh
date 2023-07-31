2421f617af6b7546286474802764da8acfa8460c
Brian Vu <emailbrian.vu@gmail.com>
2023-07-30 17:29:59 -0700

    feat: add warm_containers parameter

--- src/scripts/local_start_api.sh
+++ src/scripts/local_start_api.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ORB_STR_TEMPLATE="$(echo "${ORB_STR_TEMPLATE}" | circleci env subst)"
 ORB_STR_ENV_VARS="$(echo "${ORB_STR_ENV_VARS}" | circleci env subst)"
+
 if [ -n "$ORB_STR_TEMPLATE" ]; then
   set -- "$@" -t "$ORB_STR_TEMPLATE"
 fi
@@ -9,6 +10,10 @@ set -- "$@" -p "$ORB_INT_PORT"
 
 if [ -n "$ORB_STR_ENV_VARS" ]; then
   set -- "$@" -n "$PARAM_ENV_VARS"
+fi
+
+if [ -n "$ORB_ENUM_WARM_CONTAINERS" ]; then
+  set -- "$@"  --warm-containers "$ORB_ENUM_WARM_CONTAINERS"
 fi
 
 if [ "$ORB_BOOL_DEBUG" -eq 1 ]; then
